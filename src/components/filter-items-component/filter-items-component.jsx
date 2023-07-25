da7b793cca48efa64bb2bf8d94d107f231160ec6
Denis Ivanov <site.ivanov2014@yandex.ru>
2023-07-25 14:21:32 +0300

    добавил фильтр

--- src/components/filter-items-component/filter-items-component.jsx
+++ src/components/filter-items-component/filter-items-component.jsx
@@ -6,32 +6,41 @@ import { useSelector } from "react-redux";
 import styles from './filter-items-component.module.css'
 
 function FilterItemsComponent({ data }) {
-    const {vendor} = useParams()
+    const { vendor } = useParams()
     const filterValue = useSelector((state) => state.filter.value.value);
     const filteredData = data.filter(i => i.modelCart.toLowerCase().includes(filterValue === undefined ? '' : filterValue.toLowerCase()))
 
-    return (
+    return (vendor !== undefined ? (
         <div className={styles.container}>
             <h2>
                 Заправка картриджей {vendor === undefined ? '' : vendor.toUpperCase()}
             </h2>
-            <Filter />
+            {vendor !== undefined && <Filter />}
             <div className={styles.price_container}>
-            {filteredData.map((i, key) => {
-                return (
-                    <Item
-                        modelCart={i.modelCart}
-                        vend={i.vendor}
-                        chip={i.chip}
-                        devices={i.devices}
-                        recovery_price={i.recovery_price}
-                        refill_price={i.refill_price}
-                        key={key}
-                        examples={i.examples}
-                    />
-                )
-            })}
+                {filteredData.map((i, key) => {
+                    return (
+                        <Item
+                            modelCart={i.modelCart}
+                            vend={i.vendor}
+                            chip={i.chip}
+                            devices={i.devices}
+                            recovery_price={i.recovery_price}
+                            refill_price={i.refill_price}
+                            key={key}
+                            examples={i.examples}
+                        />
+                    )
+                })}
             </div>
+        </div>)
+        :
+        <div>
+            <h3>
+                Зачем нужна заправка картриджей?
+            </h3>
+            <p>
+                Заправка картриджей нужна чтобы заправить картридж
+            </p>
         </div>
     );
 }
