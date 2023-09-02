6196feab75db7c46c2a4a13ee762e4fb7e25bad0
Denis Ivanov <site.ivanov2014@yandex.ru>
2023-09-02 14:48:47 +0300

    main page

--- src/components/main-page component/services/repair-item/repair-item.jsx
+++ src/components/main-page component/services/repair-item/repair-item.jsx
@@ -1,31 +1,31 @@
-import styles from './refill-item.module.css'
+import styles from './repair-item.module.css'
 import { Link } from 'react-router-dom'
 
-const RefillItem = () => {
+const RepairItem = () => {
 
-    const image = require('../../../../images/refill/kyocera/TK-5220C.png')
+    const image = require('../../../../images/repair/kyocera/TASKalfa6501.png')
 
     return (
         <div className={styles.box}>
-            <h1 className={styles.title}>Заправка картриджей</h1>
+            <h1 className={styles.title}>Ремонт принтеров и МФУ</h1>
             <div className={styles.refill_box}>
-                <img className={styles.image} src={image} alt="zapravka" />
                 <div className={styles.description_and_button}>
                     <p className={styles.description_refill}>
-                        Заправка картриджей в нашей компании осуществляется с использованием тонеров лучших мировых производителей.
+                        Ремонт принтеров и МФУ в нашей компании осуществляется с использованием тонеров лучших мировых производителей.
                         В процессе заправки каждый картридж тестируется на наличие дефектов, тщательно вычищается от старого содержимого, разбирается
                         и собирается без повреждения корпуса.<br />
                         Так же, возможна доставка от клиента и обратно.
                     </p>
                     <Link
                         className={styles.button}
-                        to='/refill'
+                        to='/repair'
                     >Подробнее...
                     </Link>
                 </div>
+                <img className={styles.image} src={image} alt="zapravka" />
             </div>
         </div>
     )
 }
 
-export default RefillItem
+export default RepairItem
--- src/components/main-page component/services/services.jsx
+++ src/components/main-page component/services/services.jsx
@@ -1,32 +1,15 @@
 import styles from './services.module.css'
 import RefillItem from './refill-item/refill-item'
-import { mainServicesItems } from '../../../utils/main-services-items'
+import RepairItem from './repair-item/repair-item'
 
 const Services = () => {
 
+
+
     return (
         <div className={styles.container}>
-            <h2>Наши услуги</h2>
-            <div className={styles.box}>
-                <RefillItem
-                    title={mainServicesItems.refill.title}
-                    description={mainServicesItems.refill.description}
-                    style={styles.box0}
-                    image={mainServicesItems.refill.image}
-                    text={mainServicesItems.refill.text} />
-                <RefillItem
-                    title={mainServicesItems.repair.title}
-                    description={mainServicesItems.repair.description}
-                    style={styles.box1}
-                    image={mainServicesItems.repair.image}
-                    text={mainServicesItems.repair.text} />
-                <RefillItem
-                    title={mainServicesItems.flashing.title}
-                    description={mainServicesItems.flashing.description}
-                    style={styles.box0}
-                    image={mainServicesItems.flashing.image}
-                    text={mainServicesItems.flashing.text} />
-            </div>
+            <RefillItem />
+            <RepairItem />
         </div>
     )
 
