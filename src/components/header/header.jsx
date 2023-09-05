6ee2d359a58dac5ea412e7f8c437c530fa35daf2
Denis Ivanov <site.ivanov2014@yandex.ru>
2023-09-05 13:58:31 +0300

    header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,28 +5,23 @@ import SocialIcons from './social-icons/social-icons'
 function HeaderComponent() {
     return (
         <div className={styles.container}>
-
             <div className={styles.logo_container}>
-                <div className={styles.info_box}>
                     <NavLink
                         className={styles.link}
-                        to="/"
-                    >
+                        to="/">
                         <h1 className={styles.header}>ПРИНТРИДЖ</h1>
                     </NavLink>
                     <div>
-
                         <p>
                             Обуховской обороны, 116к1, лит. Е, 4-й этаж
                         </p>
+                        <p>fox.spb@bk.ru</p>
                     </div>
-                </div>
             </div>
             <div className={styles.phone_social_box}>
-                <a className={styles.link} href={`tel:${+7 - 953 - 368 - 18 - 36}`}>{<p className={styles.phone}>8-953-368-18-36</p>}</a>
                 <SocialIcons />
+                <a className={styles.link} href={`tel:${+7 - 953 - 368 - 18 - 36}`}>{<p className={styles.phone}>8-953-368-18-36</p>}</a>
             </div>
-
         </div>
     );
 }
