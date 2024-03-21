03b2a4c8969eaa242e2eb8cfdb75109864ad13cf
Denis Ivanov <site.ivanov201@yandex.ru>
2024-03-21 19:24:33 +0300

    Настраиваю возвращение 404

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -4,14 +4,14 @@ import { Navigate } from 'react-router-dom';
 function NotFound404() {
 
     return (
-        <>
-            <div className={styles.container}>
+
+           /* <div className={styles.container}>
                 <div className={styles.info_box}>
                     <h2 className={styles.title}>404 - Страница не найдена</h2>
                 </div>
-            </div>
+            </div>*/
             <Navigate to="/404" replace />
-        </>
+
     );
 }
 
